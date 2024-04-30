6f0e8845f63221ed26a693c7be006bf3bba30bd3
biatrisz <132359853+biatrisz@users.noreply.github.com>
2024-04-30 15:40:03 -0300

    facebook

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,4 @@
-import { View } from 'react-native';
+import { View, Image, Text } from 'react-native';
 import { styles } from '../css/style';
 import Botao from '../components/botao';
 import InputTexto from '../components/inputTexto';
@@ -6,18 +6,29 @@ import { useState } from 'react';
 
 export default function App() {
   const [valida, setValida] = useState();
-  const atualizaTexto = (newText) =>{
+  const atualizaTexto = (newText) => {
     console.log(newText);
   }
+
+
   return (
     <View style={styles.container}>
-      <InputTexto label="Digite seu nome" dica="Digite seu nome"/>
-      <InputTexto valor={atualizaTexto} label="Digite seu CPF" dica="Digite seu CPF"/>
-      <InputTexto label="Digite sua senha" dica="Digite sua senha" seguranca={true}/>
+      <Image
+        style={{ width: 300, height: 80, marginBottom: 30 }}
+        resizeMode="contain"
+        source={require('../../assets/facebook.png')}
+      />
+      <InputTexto label="Digite seu nome" dica="Digite seu nome" />
+      <InputTexto valor={atualizaTexto} label="Digite seu CPF" dica="Digite seu CPF" />
 
       <Botao btn="entrar" cor='#000' src='home' />
+      <View style={styles.cont}>
+        <View style={styles.line} />
+        <Text style={styles.text} >ou</Text>
+        <View style={styles.line}></View>
+      </View>
       <Botao btn="excluir" cor='#f00' src='excluido' />
-      <Botao btn="cadastrar" cor='#0f0' src='cadastrar' />
+
     </View>
   );
 }
